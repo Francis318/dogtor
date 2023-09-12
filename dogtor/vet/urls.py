dcc048062fede03dd0c40f8a3b6b3ab3143f0bb1
Francis318 <enriquereyes32@hotmail.com>
2023-09-11 22:05:42 -0600

    fin de clase

--- dogtor/vet/urls.py
+++ dogtor/vet/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .views import list_pet_owners, Test, OwnersList, OwnerDetail, PetsList, PetGet
 
 urlpatterns=[
-    path("owners/", OwnersList.as_view()),
+    path("owners/", OwnersList.as_view(), name="owners_list"),
     path("test/", Test.as_view()),
-    path("owners/<int:pk>", OwnerDetail.as_view()),
+    path("owners/<int:pk>", OwnerDetail.as_view(), name="owners_detail"),
     path("pets/", PetsList.as_view()),
     path("pets/<int:pk>", PetGet.as_view()),
 ]
